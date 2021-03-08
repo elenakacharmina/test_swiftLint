8a00c1fce12e10b5d6a05db6f36410101f74d84f
Elena <elena_kacharmina@mail.ru>
2021-03-08 21:26:29 +0300

    9

--- Test_SwiftLint/Test_SwiftLint/ViewController.swift
+++ Test_SwiftLint/Test_SwiftLint/ViewController.swift
@@ -11,7 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("")
+        print("class ViewController: UIViewControllerclass ")
         // Do any additional setup after loading the view.
     }
 
